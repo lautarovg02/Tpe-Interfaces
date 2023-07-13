8de9795ae43272c41f1822b975853a7984f14b41
lautarovg02 <lautarovg147@gmail.com>
2023-07-13 18:42:29 -0300

    arreglando detalles

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ const msgPassword = document.getElementById("msg-password");
 // Arreglo para almacenar usuarios registrados
 const registeredUsers = [
     {'username': "lautaro02",
-     'password': "bocademivida",
+     'password': "lautaro",
      'rol': "user",
      'email': "lautaro@gmail.com"
     },
